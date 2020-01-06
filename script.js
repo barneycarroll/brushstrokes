7f000b365eb051c8ece0cfcf3d866e6616fb46eb
Barney <barney.carroll@gmail.com>
2020-01-06 06:10:51 +0000

    Design finished, broken font

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ const Stroke = ({outer, inner}) =>
 const Ticket = () => 
   s`.Ticket
     padding .7em 1.5em
+    font-weight bold
     height 2.333333333333cm
     box-sizing border-box
     border-bottom 1px dashed #864
   `(
-    [
-      'Laura Truesdale 07854130817',
-      'brushstrokes.courses@gmail.com',
-      'http://www.brushstrokes.courses',
-    ]
-      .map(it => [it, s`br`])
+    'www.brushstrokes.courses', s`br`,
+    'brushstrokes.courses@gmail.com', s`br`,
+    s`span
+      font-weight 500
+    `('Laura Truesdale '), '07854130817', s`br`,
   )
 
 s.mount(() =>
-  s`.App.font8
+  s`.App.font4
     line-height 1.5
     display block
     position absolute
@@ -36,30 +36,56 @@ s.mount(() =>
     width 29.7cm
     height 42cm
     
-    font-size 1.7rem
+    font-size 1rem
     color #fa1
 
     background-image    url(./img/laura_portrait.jpg) 
-    background-size     22.7cm
+    background-size     23cm
     background-position 0 50%
+
+    p {
+      margin 1em 0
+    }
 	`(
-    s`img[src=./img/stroke.svg]
-      position absolute
-      
-      width 100cm
-      top   -79em
-      left  -65em
-    `,
+    Stroke({
+      outer: `
+        top: -4cm;
+        left: -7cm;
+        width: 120cm;
+        position: absolute;
+      `,
+      inner: `
+        path {
+          fill: #364 !important;
+        }
+      `
+    }),
 
     Stroke({
       outer: `
-        top: 0;
-        left: 0;
+        top: 28.5cm;
+        left: -7cm;
+        width: 120cm;
         position: absolute;
       `,
       inner: `
         path {
           fill: #348 !important;
+        }
+      `
+    }),
+
+    Stroke({
+      outer: `
+        top: -61.5cm;
+        left: -83cm;
+        width: 110cm;
+        transform: rotate(180deg);
+        position: absolute;
+      `,
+      inner: `
+        path {
+          fill: #fa1 !important;
         }
       `
     }),
@@ -72,10 +98,12 @@ s.mount(() =>
       right 0
       height 100%
       width 7cm
-      background #fed
+      background #fa1
 
       font-size .9rem
       color #444
+
+      border-left 1px solid
     `(
       Array
         .from({length: 100})
@@ -90,47 +118,113 @@ s.mount(() =>
       width 22.7cm
       padding 3rem
     `(
-      s`h1`(
-        s`span`(
+      s`h1.font1
+        margin -1rem 0 0
+        font-size 3.88rem
+      `(
+        s`span
+          border-bottom .075cm solid
+          padding-bottom .7rem
+        `(
           'BRUSHSTROKES.COURSES'
         ),
       ),
 
       s`h2`(
-        '6-Week Portraiture Course with Laura Truesdale, Artist & accredited Qualified Teacher Status.'
+        '6-week portraiture course with Qualified Teacher Status accredited artist Laura Truesdale'
       ),
 
-      s`p`(
-        'Learn how to create a portrait using different media, including a life-drawing class & an opportunity to create your own portraiture project with 1-to-1 mentoring.'
-      ),
+      s`.Footer
+        padding-top 26cm
+        display flex
 
-      s`table`(
-        s`tbody`(
-          s`tr`(
-            s`td`('08th February'), s`td`('Life Drawing'            )
+        .Box {
+          flex 1 1 50%
+          box-sizing border-box
+
+          :last-child {
+            padding-left 1em
+          }
+        }
+      `(
+        s`.Box`(
+          s`p
+            font-weight bold
+          `(
+            'Learn portraiture using different media, including a life-drawing class & an opportunity to create your own portraiture project with 1-to-1 mentoring.'
           ),
-          s`tr`(
-            s`td`('15th February'), s`td`('Sketching'               )
+
+          s`p
+            color white
+          `(
+            'Saturday Mornings, 9:00 AM – 11:30 AM', s`br`,
+            'Main Hall at The Dolphin Centre, Whitchurch Road, Pangbourne, RG8 7DA', s`br`,
+            s`span
+              font-weight bold
+            `('£200 - easels & all materials provided'),
           ),
-          s`tr`(
-            s`td`('22nd February'), s`td`('Soft Pastels'            )
-          ),
-          s`tr`(
-            s`td`('29th February'), s`td`('Acrylic Paint'           )
-          ),
-          s`tr`(
-            s`td`('07th March'   ), s`td[rowspan=2]`('Personal Project' )
-          ),
-          s`tr`(
-            s`td`('14th March'   ),
+        ),
+
+        s`.Box`(
+          s`table
+            font-size 1.333rem
+            margin .9rem 0
+            border-collapse collapse
+    
+            td {
+              padding 0
+            }
+    
+            td:nth-child(-n+2) {
+              text-align right
+            }
+    
+            td:nth-child(n+4) {
+              padding-left .5em
+            }
+
+            td:nth-child(4) {
+              color white
+            }
+          `(
+            s`tbody`(
+              s`tr`(
+                s`td`(''), s`td`('8'), s`td`('th February'), s`td`('Life Drawing'            )
+              ),
+              s`tr`(
+                s`td`('1'), s`td`('5'), s`td`('th February'), s`td`('Sketching'               )
+              ),
+              s`tr`(
+                s`td`('2'), s`td`('2'), s`td`('nd February'), s`td`('Soft Pastels'            )
+              ),
+              s`tr`(
+                s`td`('2'), s`td`('9'), s`td`('th February'), s`td`('Acrylic Paint'           )
+              ),
+              s`tr`(
+                s`td`(''), s`td`('7'), s`td`('th March'   ), s`td[rowspan=2]`('Personal Project' )
+              ),
+              s`tr`(
+                s`td`('1'), s`td`('4'), s`td`('th March'   ),
+              ),
+            ),
           ),
         ),
       ),
 
-      s`p`(
-        'Saturday Mornings, 9:00 AM – 11:30 AM', s`br`,
-        'Main Hall at The Dolphin Centre, Whitchurch Road, Pangbourne, RG8 7DA'
+      s`p
+        color #348
+        margin 0 !important
+        padding 1rem
+        font-weight bold
+        font-size 1.3rem
+        text-align center 
+      `(
+        'www.brushstrokes.courses', s`br`,
+        'brushstrokes.courses@gmail.com', s`br`,
+        s`span
+          font-weight 500
+        `('...or call Laura Truesdale on '), '07854130817', s`br`,
       ),
-  ),
+    ),
 	),
 )
